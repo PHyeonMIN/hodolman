6bf7c0c728b442a5cd079d9e309f0b4b18f86816
박현민 <dndb4902@naver.com>
2022-10-17 08:49:29 +0900

    # first commit

---
+++ src/test/java/com/hodolog/HodolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
