ce0b616e5dacd0736ec0074d18af849082b6fccc
박현민 <dndb4902@naver.com>
2022-10-18 08:46:43 +0900

    # 페이징 처리

--- src/main/java/com/hodolog/service/PostService.java
+++ src/main/java/com/hodolog/service/PostService.java
@@ -6,6 +6,7 @@ import com.hodolog.request.PostCreate;
 import com.hodolog.response.PostResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -43,10 +44,16 @@ public class PostService {
         return response;
     }
 
-    public List<PostResponse> getList() {
-       return postRepository.findAll().stream()
+    public List<PostResponse> getList(Pageable pageable) {
+        // web -> page 1 -> 0
+//        Pageable pageable = PageRequest.of(page, 5, Sort.by(Sort.Direction.DESC,"id"));
+       return postRepository.findAll(pageable).stream()
 //               .map(post -> new PostResponse(post)) // 밑과 같음
                .map(PostResponse::new)
                .collect(Collectors.toList());
     }
+
+    // 글이 너무 많은 경우 -> 비용이 너무 많이 든다.
+    // 글이 -> 1억개 -> DB에서 글을 모두 조회하는 경우 -> DB가 뻗을 수 있다.
+    // DB -> 애플리케이션 서버로 전달하는 시간, 트래픽 비용 등이 많이 발생할 수 있다.
 }
