3fdd5526e296bb699e35d2414dfb5d2c2a8f3cd8
박현민 <dndb4902@naver.com>
2022-10-17 09:47:46 +0900

    # Post 데이터 컨텐츠 타입

--- src/main/java/com/hodolog/controller/PostController.java
+++ src/main/java/com/hodolog/controller/PostController.java
@@ -1,8 +1,12 @@
 package com.hodolog.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hodolog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class PostController {
 
@@ -11,8 +15,17 @@ public class PostController {
     // SPA -> vue(vue + SSR = nuxt), react(react + SSR = next)
         // -> javascript + <-> API (JSON)
 
-    @GetMapping("/posts")
-    public String get() {
+    // Http Method
+    // GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+    // 글 등록
+    // POST Method
+
+    @PostMapping("/posts")
+//    public String post(@RequestParam String title, @RequestParam String content) {
+//    public String post(@RequestParam Map<String, String> params) {
+    public String post(@RequestBody PostCreate params) {
+//        log.info("title={}, content={}", title, content);
+        log.info("params={}",params.toString());
         return "Hello World";
     }
 }
