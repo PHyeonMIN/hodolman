2472a2d4b288820b33f0ad63e0b2c906b98b1267
박현민 <dndb4902@naver.com>
2022-10-25 09:49:45 +0900

    # 글 내용 & 수정화면

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -8,13 +8,14 @@ axios.get("/api/posts?page=1&size=5").then(response => {
     return posts.value.push(r);
   })
 });
+
 </script>
 
 <template>
   <ul>
     <li v-for="post in posts" :key="post.id">
       <div>
-        {{ post.title }}
+        <router-link :to="{ name: 'read', params: {postId: post.id}}">{{ post.title }}</router-link>
       </div>
       <div>
         {{ post.content }}
