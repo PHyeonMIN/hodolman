470449b20e7c1727bea885bbd2c2836ae5bc05ef
박현민 <dndb4902@naver.com>
2022-10-25 11:09:28 +0900

    # 스타일 수정

--- front/src/views/ReadView.vue
+++ front/src/views/ReadView.vue
@@ -13,13 +13,13 @@ const props = defineProps({
 });
 
 const post = ref({
-  id:0,
+  id: 0,
   title: "",
   content: "",
 });
 
 const moveToEdit = () => {
-  router.push({name:"edit", params: {postId: props.postId}});
+  router.push({name: "edit", params: {postId: props.postId}});
 }
 
 onMounted(() => {
@@ -30,8 +30,55 @@ onMounted(() => {
 </script>
 
 <template>
-  <h2>{{ post.title }}</h2>
-  <div>{{ post.content }}</div>
+  <el-row>
+    <el-col>
+      <h2 class="title">{{ post.title }}</h2>
 
-  <el-button type="warning" @click="moveToEdit()">수정</el-button>
+      <div class="sub d-flex">
+        <div class="category">개발</div>
+        <div class="regDate">2022-06-01 23:59:59</div>
+      </div>
+    </el-col>
+  </el-row>
+
+  <el-row class="mt-3">
+    <el-col>
+      <div class="content">{{ post.content }}</div>
+    </el-col>
+  </el-row>
+
+
+  <el-row class="mt-3">
+    <el-col>
+      <div class="d-flex justify-content-end">
+        <el-button type="warning" @click="moveToEdit()">수정</el-button>
+      </div>
+    </el-col>
+  </el-row>
 </template>
+
+<style scoped lang="scss">
+.title {
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: #383838;
+  margin:0;
+}
+.sub {
+  margin-top: 10px;
+  font-size:0.78rem;
+
+  .regDate {
+    margin-left: 10px;
+    color:#6b6b6b;
+  }
+}
+
+.content {
+  font-size: 0.95rem;
+  margin-top: 12px;
+  color: #7e7e7e;
+  white-space: break-spaces;
+  line-height: 1.5;
+}
+</style>
