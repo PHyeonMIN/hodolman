98bcd660ed225fcb61c5e1ba5eece23915f16aa0
박현민 <dndb4902@naver.com>
2022-10-26 18:20:29 +0900

    # 변수값에 따라 서로 다른 클래스 컴포넌트 호출2

--- src/main/java/com/hodolog/controller/AnimalController.java
+++ src/main/java/com/hodolog/controller/AnimalController.java
@@ -7,24 +7,36 @@ package com.hodolog.controller;
 // dog -> 멍멍
 
 import com.hodolog.service.animal.AnimalService;
-import com.hodolog.service.animal.AnimalServiceFinder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+// 1. component list 주입
+// 2. map (key:beanName , value:service)
+// 3. enum
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AnimalController {
 
-    private final AnimalServiceFinder animalServiceFinder;
+//    private final AnimalServiceFinder animalServiceFinder;
+
+    private final Map<String, AnimalService> animalServices;
 
     @GetMapping("/sound")
     public String sound(@RequestParam String type){
-        AnimalService service = animalServiceFinder.find(type);
+        log.info("animalService={}, keys={}", animalServices, animalServices.keySet());
+
+        // "CAT" -> "catService"
+        AnimalService service = animalServices.get(type.toLowerCase() + "Service");
         return service.getSound();
+//        AnimalService service = animalServiceFinder.find(type);
+//        return service.getSound();
 
 //        if(type.equals("CAT")){
 //            return new CatService().getSound();
